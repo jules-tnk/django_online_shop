885bc1d25b1d2bf7837e70a542d3fa8784e34837
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-06-30 19:29:52 +0100

    Initial commit Basic product catalog implemented

---
+++ shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+
+# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    if category_slug:  # if a category is specified
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request,
+                  'shop/product/list.html',
+                  {
+                      'category': category,
+                      'categories': categories,
+                      'products': products,
+                  })
+
+
+def product_detail(request, prod_id, slug):
+    product = get_object_or_404(Product,
+                                id=prod_id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
