f0a7f1575a0a88e093c499462beeeae236fc1f8f
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-07-03 10:02:33 +0100

    prepared for deployment

--- OnlineCommand/settings.py
+++ OnlineCommand/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-#-%++j(kzx=cb0(y(-19k8ac$ry%_gq)!31m08b8(l1stz0cgx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
